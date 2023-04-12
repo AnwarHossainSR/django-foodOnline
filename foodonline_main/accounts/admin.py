283dce6dfafdcdb9ef497ae0552b6c68b4ca432f
Restyled.io <commits@restyled.io>
2023-04-12 09:54:38 +0000

    Restyled by isort

--- foodonline_main/accounts/admin.py
+++ foodonline_main/accounts/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 from .models import User, UserProfile
-from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
 
--- foodonline_main/foodonline_main/urls.py
+++ foodonline_main/foodonline_main/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.conf import settings
 from django.conf.urls.static import static
-from . import views
 from django.contrib import admin
 from django.urls import include, path
+
+from . import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
