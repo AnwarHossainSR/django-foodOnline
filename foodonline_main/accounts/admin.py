e537b42ce50349a71bafcf2735fb824a557fbaff
Restyled.io <commits@restyled.io>
2023-04-13 04:47:55 +0000

    Restyled by isort

--- foodonline_main/accounts/admin.py
+++ foodonline_main/accounts/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .models import User
-from .models import UserProfile
+from .models import User, UserProfile
 
 # Register your models here.
 
--- foodonline_main/accounts/signals.py
+++ foodonline_main/accounts/signals.py
@@ -1,8 +1,7 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from .models import User
-from .models import UserProfile
+from .models import User, UserProfile
 
 
 @receiver(post_save, sender=User)
