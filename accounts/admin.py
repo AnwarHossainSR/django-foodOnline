ee35c286535d4531496927c8fb5b5ac5bb7cbcc1
Anwar Hossain <mahedianwar@gmail.com>
2023-09-14 16:11:32 +0600

    modify admin display pag

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
 
-from accounts.models import User
+from .models import User
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
 
-admin.site.register(User)
+
+class CustomUserAdmin(UserAdmin):
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+    list_display = ('email', 'first_name', 'last_name',
+                    'username', 'role', 'is_active',  'last_login',)
+    list_ordering = ('-date_joined',)
+
+
+admin.site.register(User, CustomUserAdmin)
