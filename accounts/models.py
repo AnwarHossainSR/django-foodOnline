69e8e9679d3cef3c5460425fe2ae8f3c53ce2a87
Anwar Hossain <mahedianwar@gmail.com>
2023-09-14 17:08:00 +0600

    implemented user profile signals

--- accounts/models.py
+++ accounts/models.py
@@ -87,9 +87,12 @@ class User(AbstractBaseUser):
 
 
 class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile', blank=True, null=True)
-    profile_picture = models.ImageField(upload_to='users/profile_pictures', blank=True, null=True)
-    cover_photo = models.ImageField(upload_to='users/cover_photos', blank=True, null=True)
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, related_name='profile', blank=True, null=True)
+    profile_picture = models.ImageField(
+        upload_to='users/profile_pictures', blank=True, null=True)
+    cover_photo = models.ImageField(
+        upload_to='users/cover_photos', blank=True, null=True)
     address_line_1 = models.CharField(max_length=100, blank=True, null=True)
     address_line_2 = models.CharField(max_length=100, blank=True, null=True)
     city = models.CharField(max_length=50, blank=True, null=True)
