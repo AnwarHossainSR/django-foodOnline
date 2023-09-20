0cceb9df5152b376abe3fc0d5c3de9c22db56b0b
Anwar Hossain <mahedianwar@gmail.com>
2023-09-20 09:23:09 +0600

    add category

--- vendor/views.py
+++ vendor/views.py
@@ -10,6 +10,8 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required, user_passes_test
 from accounts.views import check_role_vendor
 from menu.models import Category, FoodItem
+from django.template.defaultfilters import slugify
+from menu.forms import CategoryForm
 
 # Create your views here.
 
@@ -71,8 +73,30 @@ def fooditems_by_category(request, pk=None):
     }
     return render(request, 'vendor/fooditems_by_category.html', context)
 
-def add_category(request, pk=None):
-    pass
+@login_required(login_url='login')
+@user_passes_test(check_role_vendor)
+def add_category(request):
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            category_name = form.cleaned_data['category_name']
+            category = form.save(commit=False)
+            category.vendor = get_vendor(request)
+            
+            category.save() # here the category id will be generated
+            category.slug = slugify(category_name)+'-'+str(category.id) # chicken-15
+            category.save()
+            messages.success(request, 'Category added successfully!')
+            return redirect('menu_builder')
+        else:
+            print(form.errors)
+
+    else:
+        form = CategoryForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'vendor/add_category.html', context)
 
 def edit_category(request, pk=None):
     pass
