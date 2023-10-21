a736a3280576e889bc68ba97d0cb69b161b9a51f
Anwar Hossain <mahedianwar@gmail.com>
2023-10-21 21:24:42 +0600

    vendor detail section and card model

--- marketplace/views.py
+++ marketplace/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
-from vendor.models import Vendor
+from django.shortcuts import get_object_or_404, redirect, render
+from vendor.models import OpeningHour, Vendor
+from menu.models import Category, FoodItem
+from django.db.models import Prefetch
+from datetime import date, datetime
+from .models import Cart
 
 # Create your views here.
 
@@ -13,7 +17,34 @@ def marketplace(request):
     return render(request, 'marketplace/listings.html', context)
 
 def vendor_detail(request, vendor_slug):
-    pass
+    vendor = get_object_or_404(Vendor, vendor_slug=vendor_slug)
+
+    categories = Category.objects.filter(vendor=vendor).prefetch_related(
+        Prefetch(
+            'fooditems',
+            queryset = FoodItem.objects.filter(is_available=True)
+        )
+    )
+
+    opening_hours = OpeningHour.objects.filter(vendor=vendor).order_by('day', 'from_hour')
+    
+    # Check current day's opening hours.
+    today_date = date.today()
+    today = today_date.isoweekday()
+    
+    current_opening_hours = OpeningHour.objects.filter(vendor=vendor, day=today)
+    if request.user.is_authenticated:
+        cart_items = Cart.objects.filter(user=request.user)
+    else:
+        cart_items = None
+    context = {
+        'vendor': vendor,
+        'categories': categories,
+        'cart_items': cart_items,
+        'opening_hours': opening_hours,
+        'current_opening_hours': current_opening_hours,
+    }
+    return render(request, 'marketplace/vendor_detail.html', context)
 
 def add_to_cart(request, vendor_slug):
     pass
